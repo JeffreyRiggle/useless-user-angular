02ead870fe82c6eeea099e65d7c9b69eba6afb7d
Jeffrey Riggle <JeffreyRiggle@gmail.com>
2017-10-14 09:33:38 -0400

    Initial angular4 setup

---
+++ src/app/user-controller.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-controller',
+  templateUrl: './user-controller.component.html',
+  styleUrls: ['./user-controller.component.css']
+})
+export class UserControllerComponent {
+}
