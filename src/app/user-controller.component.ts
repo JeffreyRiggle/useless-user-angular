d5adfb23fa38cf7f091f42fa3b2a76e61f4f7e42
Jeffrey Riggle <JeffreyRiggle@gmail.com>
2017-10-15 09:41:21 -0400

    Fix up user controller

--- src/app/user-controller.component.ts
+++ src/app/user-controller.component.ts
@@ -6,4 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-controller.component.css']
 })
 export class UserControllerComponent {
+    valid: boolean;
+    numberOfUsers: number;
+
+    constructor() {
+        this.valid = false;
+        this.numberOfUsers = 0;
+    }
+
+    createUser() {
+        alert('number of Users ' + this.numberOfUsers);
+    }
+
+    updateNumberOfUsers($event) {
+        var newValue = Number.parseInt($event.target.value)
+        this.valid = !Number.isNaN(newValue);
+        if (this.valid) {
+            this.numberOfUsers = newValue;
+        }
+    }
 }
